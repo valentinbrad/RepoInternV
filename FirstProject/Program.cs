19dba63944d466ba7104e0312a526da6d071ccc3
valentinbrad <valy_brad@yahoo.com>
2015-07-06 14:35:47 +0300

    prb3

---
+++ FirstProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            float number = 0.0f;
+            Console.Write("Give a number: " );
+            string stringNr=Console.ReadLine();
+            Console.WriteLine("String number is "+stringNr);
+            number=functionConvert(stringNr);
+            Console.WriteLine("Integer number is " + number);
+            Console.ReadKey();
+        }
+        static public float functionConvert(string stringNr)
+        {
+            bool pas = false;
+            bool negative=false;
+            string stringNumber = stringNr;
+            char minus = '-';
+            float integerNumber = 0.0f;
+            
+            for (int i = 0; i < stringNumber.Length; i++)
+            {
+                if (pas == false && i==0)
+                    if (stringNumber[0].CompareTo(minus) == 0)
+                    {
+                        negative = true;
+                        pas = true;
+                        i++;
+                    }
+                integerNumber = integerNumber * 10;
+                integerNumber = integerNumber + stringNumber[i] - 48;
+            }
+            if (negative)
+                integerNumber = integerNumber * -1;
+            return integerNumber;
+        }
+
+    }
+}
+
