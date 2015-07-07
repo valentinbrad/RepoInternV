a965fe96f7a3a7012236882f6ad61c59896dd4b1
valentinbrad <valy_brad@yahoo.com>
2015-07-07 16:13:20 +0300

    prb9 varianta 1

---
+++ Project3/SecretSanta.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Project3
+{
+    partial class SecretSanta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewPeople = new System.Windows.Forms.ListView();
+            this.NameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SurnameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonChoose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SecretSantaColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewPeople
+            // 
+            this.listViewPeople.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewPeople.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameColumn,
+            this.SurnameColumn,
+            this.SecretSantaColumn});
+            this.listViewPeople.Location = new System.Drawing.Point(6, 19);
+            this.listViewPeople.Name = "listViewPeople";
+            this.listViewPeople.Size = new System.Drawing.Size(292, 266);
+            this.listViewPeople.TabIndex = 1;
+            this.listViewPeople.UseCompatibleStateImageBehavior = false;
+            this.listViewPeople.View = System.Windows.Forms.View.Details;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.Text = "Name";
+            this.NameColumn.Width = 70;
+            // 
+            // SurnameColumn
+            // 
+            this.SurnameColumn.Text = "Surname";
+            this.SurnameColumn.Width = 70;
+            // 
+            // buttonChoose
+            // 
+            this.buttonChoose.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonChoose.Location = new System.Drawing.Point(329, 72);
+            this.buttonChoose.Name = "buttonChoose";
+            this.buttonChoose.Size = new System.Drawing.Size(99, 39);
+            this.buttonChoose.TabIndex = 6;
+            this.buttonChoose.Text = "Find SecretSanta";
+            this.buttonChoose.UseVisualStyleBackColor = true;
+            this.buttonChoose.Click += new System.EventHandler(this.buttonChoose_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.listViewPeople);
+            this.groupBox1.Location = new System.Drawing.Point(11, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 295);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose Secret Santa";
+            // 
+            // SecretSantaColumn
+            // 
+            this.SecretSantaColumn.Text = "SecretSanta";
+            this.SecretSantaColumn.Width = 140;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonRefresh.Location = new System.Drawing.Point(330, 135);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(99, 39);
+            this.buttonRefresh.TabIndex = 7;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonExit.Location = new System.Drawing.Point(330, 196);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(99, 39);
+            this.buttonExit.TabIndex = 8;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // SecretSanta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 313);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonChoose);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SecretSanta";
+            this.Text = "SecretSanta";
+            this.Load += new System.EventHandler(this.SecretSanta_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewPeople;
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.ColumnHeader SurnameColumn;
+        private System.Windows.Forms.Button buttonChoose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ColumnHeader SecretSantaColumn;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
