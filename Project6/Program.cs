52957e89879750b646bd312a6603dd35fe59efa3
valentinbrad <valy_brad@yahoo.com>
2015-07-09 10:19:15 +0300

    prb6 v2

--- Project6/Program.cs
+++ Project6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,57 +13,72 @@ namespace Project6
         {
             List<string> letters = new List<string>();
             List<string> goodWords = new List<string>();
-            string[] lines = readFile();
-            
+
             Console.Write("Give first word: ");
             string firstWord = giveWord();
             Console.Write("Give second word: ");
             string secondWord = giveWord();
+            while(!(firstWord.Length == secondWord.Length)){
+                Console.WriteLine("Words do not have the same length!");
+                Console.Write("Give first word: ");
+                firstWord = giveWord();
+                Console.Write("Give second word: ");
+                secondWord = giveWord();
+            }
             
+                for (int i = 0; i < secondWord.Length; i++)
+                    letters.Add(secondWord.Substring(i, 1));
 
-            for(int i=0;i<secondWord.Length;i++)
-                letters.Add(secondWord.Substring(i,1));
-
-            foreach (string line in lines)
-                if (line.Length == firstWord.Length)
-                    for (int i = 0; i < letters.Count; i++)
-                        if (line.Contains(letters[i]))
-                            goodWords.Add(line);
-            Console.Write(firstWord);
-           
-            int index = 0;
-            bool semafor = false;
-            while(!firstWord.Equals(secondWord))
-            {
-                semafor = false;
-                string copy = firstWord;
-                copy = copy.Replace(copy.Substring(index,1), letters[index]);
-                foreach (string word in goodWords)
+                using (StreamReader sr = new StreamReader(@"C:\Users\valentin.brad\Documents\Visual Studio 2013\Projects\Project6\Project6\Words.txt"))
                 {
-                    if (word.Equals(copy))
+                    String line = sr.ReadLine();
+                    while (!sr.EndOfStream)
                     {
-                        firstWord = word;
-                        semafor = true;
-                        Console.Write("->");
-                        Console.Write(firstWord);                        
+                        if (line.Length == firstWord.Length)
+                            for (int i = 0; i < letters.Count; i++)
+                                if (line.Contains(letters[i]))
+                                    goodWords.Add(line);
+                        line = sr.ReadLine();
+                    }
+                }
+
+
+                Console.Write(firstWord);
+
+                int index = 0;
+                bool semafor = false;
+                while (!firstWord.Equals(secondWord))
+                {
+                    semafor = false;
+                    string copy = firstWord;
+                    copy = copy.Replace(copy.Substring(index, 1), letters[index]);
+                    foreach (string word in goodWords)
+                    {
+                        if (word.Equals(copy))
+                        {
+                            firstWord = word;
+                            semafor = true;
+                            Console.Write("->");
+                            Console.Write(firstWord);
+                        }
+                        if (semafor)
+                            break;
+                        else
+                            continue;
+
                     }
                     if (semafor)
-                        break;
-                    else                   
-                        continue;                 
+                        index++;
+                    else
+                        index++;
+                    if (index == letters.Count && (!firstWord.Equals(secondWord)))
+                    {
+                        index = 0;
+                    }
 
                 }
-                if (semafor)
-                       index++;                   
-                else                                      
-                        index++;
-                if (index == letters.Count && (!firstWord.Equals(secondWord)))
-                {
-                    index = 0;
-                }
-                
-            }            
-            Console.ReadKey();
+                Console.ReadKey();
+          
         }
 
 
@@ -73,12 +89,7 @@ namespace Project6
         }
 
 
-        static string[] readFile()
-        {
-            string fileName = @"C:\Users\valentin.brad\Documents\Visual Studio 2013\Projects\Project6\Project6\Words.txt";
-            string[] lines = System.IO.File.ReadAllLines(fileName);
-            return lines;
-        }
+      
 
 
     }
