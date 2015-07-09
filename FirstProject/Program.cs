77afa3eb3eec359b9f77ae76eb32fb96e1036ea3
valentinbrad <valy_brad@yahoo.com>
2015-07-09 10:40:51 +0300

    prb3 v2

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -11,7 +11,7 @@ namespace FirstProject
         static void Main(string[] args)
         {
            
-            float number = 0.0f;
+            int number = 0;
             Console.Write("Give a number: " );
             string stringNr=Console.ReadLine();
             Console.WriteLine("String number is "+stringNr);
@@ -19,21 +19,20 @@ namespace FirstProject
             Console.WriteLine("Integer number is " + number);
             Console.ReadKey();
         }
-        static public float functionConvert(string stringNr)
+        static public int functionConvert(string stringNr)
         {
-            bool pas = false;
+            
             bool negative=false;
             string stringNumber = stringNr;
             char minus = '-';
-            float integerNumber = 0.0f;
+            int integerNumber = 0;
             
             for (int i = 0; i < stringNumber.Length; i++)
             {
-                if (pas == false && i==0)
+                if (i==0)
                     if (stringNumber[0].CompareTo(minus) == 0)
                     {
-                        negative = true;
-                        pas = true;
+                        negative = true;                     
                         i++;
                     }
                 integerNumber = integerNumber * 10;
